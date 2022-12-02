4f268984431f8eedc39825596a0626c8722c1bdf
Victoria <mariagrisi@cc.ci.ufpb.br>
2022-12-01 23:09:57 -0300

    Arquivos de Sockets incompletos

---
+++ src/Server.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) {
+        try (
+            ServerSocket server = new ServerSocket(6666);
+            Socket client = server.accept();
+            PrintWriter
+            
+        )
+        {
+
+        }
+    }
+}
