41e5f21cd32025da42a6a498bae8b2b0a6a998d4
Samantha Medeiros <samanthadmedeiros37@gmail.com>
2022-12-08 21:48:59 -0300

    Update RaceTrack.java

--- RaceTrack.java
+++ RaceTrack.java
@@ -3,9 +3,10 @@ public class RaceTrack {
 	private int size; // dimensão da pista de corrida
 	private int horses; // número de cavalos competindo
 	
-	public int getRaceTrackSize() { return size;}
+	public int getRaceTrackSize() { return size; }
 	public void setRaceTrackSize(int size) { this.size = size; }
 	
+	// Recebe a quantidade de cavalos que estão correndo, é interessante recuperar utilizando horselist.size()
 	public int getRaceTrackHorses() { return horses; }
 	public void setRaceTrackHorses(int horses) { this.horses = horses; }
 	
