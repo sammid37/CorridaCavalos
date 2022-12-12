05e343689bea74b174d2673afcd6e257ca8c4a56
Samantha Medeiros <samanthadmedeiros37@gmail.com>
2022-12-11 23:59:10 -0300

    Update BookMaker.java

--- BookMaker.java
+++ BookMaker.java
@@ -1,99 +1,71 @@
 public class BookMaker {
-    private int bets_num = 0;
-    private float total_value = 0;
-    private float prize;
-    private float bookmaker_profit;
+	private int bets_num;
+	private float total_value;
+	private float prize;
+	private float bookmaker_profit;
+	private Server server;
 
-    // Get e set da quantidade de apostas
-    public int getBets_num() {
-        return bets_num;
-    }
-    private void setBets_num(int bets_num) {
-        this.bets_num = bets_num;
-    }
+	// Quantidade total de apostas
+	public int getBets_num() { return bets_num; }
+	private void setBets_num(int bets_num) { this.bets_num = bets_num; }
 
-    // Get e set do valor da aposta
-    public float getTotal_value() {
-        return total_value;
-    }
-    private void setTotal_value(float total_value) {
-        this.total_value = total_value;
-    }
+	// Valor total das apostas
+	public float getTotal_value() { return total_value; }
+	private void setTotal_value(float total_value) { this.total_value = total_value; }
 
-    // Get e set do lucro possivel lucro dos apostadores
-    public float getPrize() {
+	// Lucro possivel dos apostadores
+	public float getPrize() { return prize; }
+	private void setPrize(float prize) { this.prize = prize; }
 
-        return prize;
-    }
-    private void setPrize(float prize) {
-        this.prize = prize;
-    }
+	// Lucro da casa de apostas
+	public float getBookmaker_profit() { return bookmaker_profit; }
+	private void setBookmaker_profit(float bookmaker_profit) { this.bookmaker_profit = bookmaker_profit; }
+	
+	// Construtor
+	BookMaker (Server server) { this.server = server; }
+	
+	// Recu
+	public void bet(Gambler gambler, Horse horse) {
+		// Verifica se o saldo >= valor da aposta 
+		if (gambler.getWallet() >= gambler.getBet()) {
+			// Atualiza o valor da carteira (carteira - valor aposta)
+			gambler.setWallet(gambler.getWallet() - gambler.getBet());
 
-    // Get e set do lucro da casa de aposta
-    public float getBookmaker_profit() {
+			// Atualiza o valor de aposta e o nº de apostas no cavalo apostado 
+			horse.setTotal_bet_value(horse.getTotal_bet_value() + gambler.getBet());
+			horse.setBets_num(horse.getBets_num() + 1);
 
-        return bookmaker_profit;
-    }
-    private void setBookmaker_profit(float bookmaker_profit) {
-        this.bookmaker_profit = bookmaker_profit;
-    }
-    //-------------------------------------------------------------------------------------------
-    public void bet(Gambler gambler, Horse horse, int value) {
-        if (gambler.getWallet() >= value){
-            gambler.setBet(value);
-            gambler.setHorse(horse);
-            gambler.setWallet(gambler.getWallet() - value);
+			// Recupera o número e o valor total de apostas para a casa de apostas
+			this.setBets_num(this.getBets_num() + 1);
+			this.setTotal_value(this.getTotal_value() + gambler.getBet());
 
-            horse.setTotal_bet_value(horse.getTotal_bet_value() + value);
-            horse.setBets_num(horse.getBets_num() + 1);
+			// Recompensa total da Casa de Aposta
+			this.setPrize((float) (this.getPrize() + (gambler.getBet() * 0.95)));
+			
+			// Lucro da casa de apostas
+			this.setBookmaker_profit((float) (this.getBookmaker_profit() + (gambler.getBet() * 0.05)));
+		}
+		else {
+			server.writer.println("Saldo insuficiente!");
+		}
+	}
 
-            this.setBets_num(this.getBets_num() + 1);
-            this.setTotal_value(this.getTotal_value() + value);
+	// public void prize(){
+			
+	// }
 
-            this.setPrize((float) (getTotal_value() * 0.95));
-            this.setBookmaker_profit(getTotal_value() - getPrize());
-        }
-        else{
-            System.out.println("Saldo insuficiente!");
-        }
-    }
-
-    public  void result(Gambler gambler){
-        if(gambler.getBet() != 0){
-            if(this.getBets_num() >= 2) {
-                if(gambler.isWinner()) {
-                    gambler.setReward((this.getPrize()/gambler.getHorse().getTotal_bet_value())*gambler.getBet());
-                    gambler.setWallet(gambler.getWallet() + gambler.getReward());
-                    this.clear(gambler);
-                }
-                else {
-                    System.out.println("Você perdeu!!");
-                    this.clear(gambler);
-                }
-            }
-            else {
-                System.out.println("Número de apostadores insuficiente, o valor apostado será estornado!");
-                gambler.setWallet(gambler.getWallet() + gambler.getBet());
-                this.clear(gambler);
-            }
-        }
-
-    }
-
-    public void clear(Gambler gambler){
-        gambler.setReward(0);
-        gambler.setBet(0);
-        gambler.setHorse(null);
-        gambler.setWinner(false);
-    }
-
-    public void clear(Horse horse){
-         horse.setTotal_bet_value(0);
-         horse.setBets_num(0);
-    }
-
-    public void clear(){
-         this.setTotal_value(0);
-         this.setBets_num(0);
-    }
+	// Informa se um jogador ganhou ou perdeu a aposta
+	public void result(Gambler gambler) {
+		// A aposta tem de ser != 0
+		if(gambler.getBet() != 0){
+			if(gambler.isWinner()) {
+				// Reparta o prêmio para o(s) ganhador(es)
+				gambler.setReward((this.getPrize() / gambler.getHorse().getTotal_bet_value()) * gambler.getBet());
+				gambler.setWallet(gambler.getWallet() + gambler.getReward());
+			}
+			else {
+				server.writer.println("Você perdeu!!");
+			}
+		}
+	}
 }
