07361d1efe762aa3ca45993a5981a0c64c3fb51d
Samantha Medeiros <samanthadmedeiros37@gmail.com>
2022-12-11 23:57:45 -0300

    Update Gambler.java

--- Gambler.java
+++ Gambler.java
@@ -9,64 +9,35 @@ public class Gambler {
   private Horse horse; // cavalo da aposta
 
   // Get e set do nome do apostador
-  public String getName() {
-    return name;
-  }
-
-  private void setName(String name) {
-    this.name = name;
-  }
+  public String getName() { return name; }
+  private void setName(String name) { this.name = name; }
 
   // Get e set da carteira(saldo) do apostador
-  public float getWallet() {
-    return wallet;
-  }
-
-  protected void setWallet(float wallet) {
-    this.wallet = wallet;
-  }
+  public float getWallet() { return wallet; }
+  protected void setWallet(float wallet) { this.wallet = wallet; }
 
   // Get e set do valor da aposta do apostador
-  public float getBet() {
-    return bet;
-  }
-
-  protected void setBet(float bet) {
-    this.bet = bet;
-  }
+  public float getBet() { return bet; }
+  protected void setBet(float bet) { this.bet = bet; }
 
   // Get e set do recompensa do apostador
-  public float getReward() {
-    return reward;
-  }
-
-  protected void setReward(float reward) {
-    this.reward = reward;
-  }
+  public float getReward() { return reward; }
+  protected void setReward(float reward) { this.reward = reward; }
 
   // Get e set estado do jogador
-  public Boolean isWinner() {
-    return winner;
-  }
-
-  protected void setWinner(Boolean winner) {
-    this.winner = winner;
-  }
+  public Boolean isWinner() { return winner; }
+  protected void setWinner(Boolean winner) { this.winner = winner; }
 
   // Get e set do cavalo do apostador
-  public Horse getHorse() {
-    return horse;
-  }
-
-  private void setHorse(Horse horse) {
-    this.horse = horse;
-  }
+  public Horse getHorse() { return horse; }
+  private void setHorse(Horse horse) { this.horse = horse; }
 
   // Construtor
   public Gambler(Horse horse, Server server) {
     Scanner sc = new Scanner(System.in);
-    // Informando atributos do apostador
-    server.writer.println("\nNome do jogador: ");
+
+    setWinner(false); // true quando o cavalo apostado cruza a linha de chegada em 1º lugar 
+    server.writer.println("\n>> Nome do jogador: ");
     server.writer.println("WAITFORANSWER");
     try {
       setName(server.reader.readLine());
@@ -74,25 +45,41 @@ public class Gambler {
       e.printStackTrace();
     }
 
-    server.writer.println("\nSaldo: ");
+    server.writer.println("\n>> Saldo: ");
     server.writer.println("WAITFORANSWER");
     try {
       setWallet(Float.parseFloat(server.reader.readLine()));
     } catch (Exception e) {
       e.printStackTrace();
     }
-
-    server.writer.println("\nValor da aposta: ");
-    server.writer.println("WAITFORANSWER");
-    try {
-      setBet(Float.parseFloat(server.reader.readLine()));
-    } catch (Exception e) {
-      e.printStackTrace();
+   
+    // Verifica se o valor da aposta excede o valor da carteira
+    Boolean valid_value = false;
+    while (valid_value == false) {
+      server.writer.println("\n>> Valor da aposta: ");
+      server.writer.println("WAITFORANSWER");
+      
+      try {
+        float bet_value = Float.parseFloat(server.reader.readLine());
+        // Precisa ser um número positivo e <= ao saldo
+        if (bet_value <= this.getWallet() && bet_value != 0) {
+          setBet(bet_value);
+          valid_value = true;
+          break;
+        } else {
+          server.writer.println("O valor informado excede o valor da carteira. Tente novamente.");
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
-    
 
-    setHorse(horse);
-    server.writer.println("\nOlá, " + getName() + "! Você apostou R$" + getBet() + " no cavalo " + horse.getHorseName() + ".");
+    setHorse(horse);  // Seta o cavalo quando o valor da aposta é aceito.
+
+    // Mensagem do processamento
+    server.writer.println("\nOlá, " 
+    + getName() + "! Você apostou R$" 
+    + getBet() + " no cavalo " 
+    + horse.getHorseName() + ".");
   }
-
 }
