419e743d3b64c1ece00cb77a7427227824db530a
Samantha Medeiros <samanthadmedeiros37@gmail.com>
2022-12-11 23:58:16 -0300

    Update Horse.java

--- Horse.java
+++ Horse.java
@@ -8,63 +8,68 @@ public class Horse implements Runnable {
   private int bets_num;
   private float total_bet_value;
   private Server server;
+  private Boolean winner; // inicializado com false
 
-  public float getTotal_bet_value() {
-    return total_bet_value;
-  }
-  
-  public void setTotal_bet_value(float total_bet) {
-    this.total_bet_value = total_bet;
-  }
 
-  public int getBets_num() {
-    return bets_num;
-  }
-  
-  public void setBets_num(int bets_num) {
-    this.bets_num = bets_num;
-  }
+  // Valor total de apostas em um cavalo
+  public float getTotal_bet_value() { return total_bet_value; }
+  public void setTotal_bet_value(float total_bet) { this.total_bet_value = total_bet; }
 
-  public String getHorseName() {
-    return name;
-  }
+  // Total de apostas em um cavalo
+  public int getBets_num() { return bets_num; }
+  public void setBets_num(int bets_num) { this.bets_num = bets_num; }
 
-  public void setHorseName(String name) {
-    this.name = name;
-  }
+  // Id do cavalo
+  public int getHorseId() { return id; }
+  private void setHorseId(int id) { this.id = id; }
 
-  public int getHorseId() {
-    return id;
-  }
+  // Nome do cavalo
+  public String getHorseName() { return name; }
+  public void setHorseName(String name) { this.name = name; }
 
-  private void setHorseId(int id) {
-    this.id = id;
-  }
+  // Se o cavalo ganhou a corrida ou não
+  public Boolean isWinner() { return winner; }
+  protected void setWinner(Boolean winner) { this.winner = winner; }
 
+  // Construtor
   Horse(int idHorse, String nameHorse, Server server) {
     setHorseId(idHorse);
     setHorseName(nameHorse);
+    setWinner(false); // true quando ganha a corrida em 1º lugar 
     this.server = server;
   }
 
+  // Método que retorna a mensagem da thread (Thread-0: <nome> <metros percorridos>)
   static void threadMessage(String message, Server server) {
     String threadName = Thread.currentThread().getName();
     String format = "%s: %s%n";
+
     System.out.format("%s: %s%n",
-        threadName,
-        message);
+      threadName,
+      message
+    );
+
     server.writer.format(format, threadName, message);
   }
 
+  // Método run, acionado quando a thread é inicializada
   public void run() {
     try {
+      // Enquanto não percorrer a distância da posta, ele continua a correr
       while (distanceTravelled < 100) {
-        Thread.sleep(4000);
-        distanceTravelled += this.gallop();
+        Thread.sleep(4000); // a thread dorme por 4s
+        distanceTravelled += this.gallop(); // atualiza a distancia percorrida
 
+        // Verifica se cruzou a linha de chegada
         if (distanceTravelled < 100) {
           threadMessage(name + " percorreu " + distanceTravelled + " m.", server);
         } else {
+          // Se o cavalo ficar em 1º lugar, o cavalo é declarado como vencedor
+          if (place == 1) {
+            setWinner(true); 
+          }
+
+          // Quando chega na linha de chegada, informa a colocação do cavalo na corrida
           threadMessage(name + " cruzou a linha de chegada em " + place + "º lugar", server);
           place++;
         }
